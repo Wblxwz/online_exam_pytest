59a0ca9cb69f1a9c649ebf9def80fdc04dead139
wblxwz <2394559659@qq.com>
2025-10-23 23:57:16 +0800

    添加addclass

--- test_teacher.py
+++ test_teacher.py
@@ -3,14 +3,18 @@ import requests
 
 class TestTeacher:
     def setup_method(self):
-        self.teacher = Teacher('1','1')
+        self.teacher = Teacher('1','1',17)
         self.teacher.logger.info('---start test_login---')
     def test_login(self):
-        self.teacher.login()
-        code = self.teacher.response.json()['code']
+        response = self.teacher.login()
+        code = response.json()['code']
         assert code == 2002
+        assert response.json()['user']['user_id'] == self.teacher.username
     def test_addclass(self):
-        self.teacher.addClass()
+        response = self.teacher.addClass()
+        assert response.json()['code'] == 2004
+        assert response.json()['message'] == '插入class成功'
+        self.setup_method 
     def teardown_method(self):
         self.teacher.logger.info('---end test_login---')
         
--- tool.py
+++ tool.py
@@ -1,33 +1,43 @@
 import logging
 import requests
+import json
 #import time
 
 class User:
-    def __init__(self, username, password,role):
+    def __init__(self, username, password,role,id):
         self.username = username
         self.password = password
         self.role = role
+        self.id = id
         self.logger = logging.getLogger(f'{self.role}_{self.username}')
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(logging.DEBUG)
         # handler = logging.FileHandler(f'./log/{self.role}_{time.time()}.log')
         # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         # handler.setFormatter(formatter)
         # self.logger.addHandler(handler)
     def login(self):
         self.logger.info(f"{self.role} {self.username} login") 
-        self.response = requests.get(f'http://localhost:7000/login/{self.username}/{self.password}')
-        self.logger.info(self.response.content)
+        response = requests.get(f'http://localhost:7000/login/{self.username}/{self.password}')
+        self.logger.info(response.content)
+        return response
         
 class Student(User):
-    def __init__(self, username, password):
-        super().__init__(username,password,'student')
+    def __init__(self, username, password,id):
+        super().__init__(username,password,'student',id)
 
 class Teacher(User):
-    def __init__(self, username, password):
-        super().__init__(username,password,'teacher')
+    def __init__(self, username, password,id):
+        super().__init__(username,password,'teacher',id)
     def addClass(self):
         self.logger.info(f'{self.username} addclass')
-        
+        data = {
+            "class_code": "cs", 
+            "class_name": "cs222",
+            "tid": self.id
+        }
+        response = requests.post(url='http://localhost:7000/cclass',json=data)
+        self.logger.info(response.content)
+        return response
 class Root(User):
-    def __init__(self, username, password):
+    def __init__(self, username, password,id):
         super().__init__(username,password,'root')
